01aeab928dbc698cffe7ce551f1a98801e88cba0
Milosz Golebiowski <73059737+golab368@users.noreply.github.com>
2021-01-04 20:00:23 +0100

    Calkowicie poprawione zadanie z Twoja pomoca

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
-
-# INSTRUCTION
-# If you want to use Methods = DELETE, or POST the instruction is below
-# DELETE copy to terminal : curl -i -H "Content-Type: Application/json" -X DELETE http://localhost:5000/movies/0  <---- The number represents id number of the movie you want to DELETE
-# POST: curl -i -H "Content-Type: Application/json" -X POST http://localhost:5000/movies <---- ADD a new movie to library
-# PUT: curl -i -H "Content-Type: Application/json" -X PUT http://localhost:5000/movies/0 <---- The number represents id number of the movie you want to PUT new "description"
-
 
 app = Flask(__name__)
 
@@ -40,21 +33,22 @@ def get_movie():
     return jsonify({"Movies": movie_library})
 
 
-@app.route("/movies/<int:movie_id>", methods=["GET"])
-def get_movie_by_movie_id(movie_id):
-    return jsonify({"course": movie_library[movie_id]})
+@app.route("/movies/<string:title>", methods=["GET"])
+def get_movie_by_movie_title(title):
+    for movie in movie_library:
+        if title == movie["title"]:
+            return jsonify({"Movies": movie})
+    return "There is not such a movie in my library"
 
 
 @app.route("/movies", methods=["POST"])
 def add_new_movie_to_library():
-    movie = {
-        'title': 'The Holiday',
-        'movie_id': '3',
-        'genres': "romance, comedy"
-    },
 
+    movie = {'title': request.json["title"],
+             'movie_id': request.json["movie_id"],
+             'genres': request.json["genres"], }
     movie_library.append(movie)
-    return jsonify({"Created": movie})
+    return jsonify({"Created": movie_library})
 
 
 @app.route("/movies/<int:movie_id>", methods=["PUT"])
@@ -74,8 +68,3 @@ def movie_delete_by_movie_id(movie_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-# INSTRUCTION
-# If you want to use Methods = DELETE, or POST the instruction is below
-# DELETE copy to terminal : curl -i -H "Content-Type: Application/json" -X DELETE http://localhost:5000/movies/0  <---- The number represents id number of the movie you want to DELETE
-# POST: curl -i -H "Content-Type: Application/json" -X POST http://localhost:5000/movies <---- ADD a new movie to library
-# PUT: curl -i -H "Content-Type: Application/json" -X PUT http://localhost:5000/movies/0 <---- The number represents id number of the movie you want to PUT new "description"
